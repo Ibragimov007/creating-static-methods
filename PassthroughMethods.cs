127d0a72270b5d075d6ae6e9271898437809ba9f
Ibragimov007 <135506182+Ibragimov007@users.noreply.github.com>
2023-06-04 15:27:56 +0500

    Add files via upload
    Main programs about statis methods

---
+++ PassthroughMethods.cs
@@ -0,0 +1,85 @@
+﻿using System.Net.NetworkInformation;
+
+namespace CreatingMethods
+{
+    // ReSharper disable once IdentifierTypo
+    public static class PassthroughMethods
+    {
+        public static bool ReturnValue(bool boolValue)
+        {
+            return boolValue;
+        }
+
+        // TO DO #1: Add a static method here with name "ReturnValue" that gets "byteValue" parameter ("byte" type) and returns it.
+        public static byte ReturnValue(byte byteValue)
+        {
+            return byteValue;
+        }
+
+        // TO DO #2: Add a static method here with name "ReturnValue" that gets "sbyteValue" parameter ("sbyte" type) and returns it.
+        public static sbyte ReturnValue(sbyte sbyteValue)
+        {
+            return sbyteValue;
+        }
+
+        // TO DO #3: Add a static method here with name "ReturnValue" that gets "charValue" parameter ("char" type) and returns it.
+        public static char ReturnValue(char charValue)
+        {
+            return charValue;
+        }
+
+        // TO DO #4: Add a static method here with name "ReturnValue" that gets "decimalValue" parameter ("decimal" type) and returns it.
+        public static decimal ReturnValue(decimal decimalValue)
+        {
+            return decimalValue;
+        }
+
+        // TO DO #5: Add a static method here with name "ReturnValue" that gets "doubleValue" parameter ("double" type) and returns it.
+        public static double ReturnValue(double doubleValue)
+        {
+            return doubleValue;
+        }
+
+        // TO DO #6: Add a static method here with name "ReturnValue" that gets "floatValue" parameter ("float" type) and returns it.
+        public static float ReturnValue(float floatValue)
+        {
+            return floatValue;
+        }
+
+        // TO DO #7: Add a static method here with name "ReturnValue" that gets "intValue" parameter ("int" type) and returns it.
+        public static int ReturnValue(int intValue)
+        {
+            return intValue;
+        }
+
+        // TO DO #8: Add a static method here with name "ReturnValue" that gets "uintValue" parameter ("uint" type) and returns it.
+        public static uint ReturnValue(uint uintValue)
+        {
+            return uintValue;
+        }
+
+        // TO DO #9: Add a static method here with name "ReturnValue" that gets "longValue" parameter ("long" type) and returns it.
+        public static long ReturnValue(long longValue)
+        {
+            return longValue;
+        }
+
+        // TO DO #10: Add a static method here with name "ReturnValue" that gets "ulongValue" parameter ("ulong" type) and returns it.
+        public static ulong ReturnValue(ulong ulongValue)
+        {
+            return ulongValue;
+        }
+
+        // TO DO #11: Add a static method here with name "ReturnValue" that gets "shortValue" parameter ("short" type) and returns it.
+        public static short ReturnValue(short shortValue)
+        {
+            return shortValue;
+        }
+
+        // TO DO #12: Add a static method here with name "ReturnValue" that gets "ushortValue" parameter ("ushort" type) and returns it.
+        public static ushort ReturnValue(ushort ushortValue)
+        {
+            return ushortValue;
+        }
+    }
+}
